b928283bcb58ba91849d68d52620b42829ebc5bc
Suhan376 <zendev192223@gmail.com>
2025-04-17 02:41:06 +0530

    s3 added

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,13 @@
 export default {
   reactStrictMode: true,
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname:"macro-s3-images.s3.us-east-1.amazonaws.com",
+        port:"",
+        pathname: "/**",
+      }
+    ]
+  }
 };
